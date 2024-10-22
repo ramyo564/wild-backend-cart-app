185a6419f025251700d0a35a8684bd9432e19721
ramyo564 <103474568+ramyo564@users.noreply.github.com>
2024-10-22 23:27:02 +0900

    mongoDB 환경설정 분리

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,8 +14,17 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
     @Bean
-    public MongoClient mongoClient(){
-        String mongoURL = "mongodb://localhost:27017";
-        return MongoClients.create(mongoURL);
+    public MongoClient mongoClient(
+            @Value("${mongodb.url}") String url
+    ){
+        return MongoClients.create(url);
+    }
+
+    @Bean
+    public MongoDatabase mongoDatabase(
+            MongoClient mongoClient,
+            @Value("${mongodb.database}") String database
+    ){
+        return mongoClient.getDatabase(database);
     }
 }
