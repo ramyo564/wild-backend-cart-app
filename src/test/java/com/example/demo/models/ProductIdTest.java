fb97daf694e4d98f621f6221b44ab20329a6b976
ramyo564 <yohan032yohan@gmail.com>
2024-11-29 13:09:08 +0900

    ProductId 테스트코드 추가
    ID의 고유성이나 형식에 대한 추가 검증 추가

--- src/test/java/com/example/demo/models/ProductIdTest.java
+++ src/test/java/com/example/demo/models/ProductIdTest.java
@@ -11,4 +11,20 @@ class ProductIdTest {
         System.out.println("Generated ID: " + id.id());
         assertNotNull(id.id(), "ID should not be null");
     }
+    @Test
+    void createMultipleCartItemIds_ShouldGenerateUniqueIds() {
+        CartItemId id1 = new CartItemId();
+        CartItemId id2 = new CartItemId();
+
+        assertNotEquals(id1.id(), id2.id(), "Generated IDs should be unique");
+    }
+
+    @Test
+    void generatedId_ShouldFollowExpectedFormat() {
+        CartItemId id = new CartItemId();
+        String generatedId = id.id();
+
+        // ID 형식이 예상된 패턴을 따르는지 확인
+        assertTrue(generatedId.matches("^[a-zA-Z0-9-]+$"), "ID should match expected format");
+    }
 }
